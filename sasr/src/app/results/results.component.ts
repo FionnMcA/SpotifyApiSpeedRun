2ad74b222e7f261d6ac2b0049be5dd5d14442452
FMcA <f.mcardle1@universityofgalway.ie>
2024-05-15 22:08:00 +0100

    cleanup

--- sasr/src/app/results/results.component.ts
+++ sasr/src/app/results/results.component.ts
@@ -47,50 +47,6 @@ export class ResultsComponent implements OnInit {
     this.getTopArtists();
   }
 
-  HandleHash() {
-    const hash = window.location.hash.substring(1);
-    if (hash) {
-      const tokens = this.parseFragment(hash);
-      if (tokens.access_token && tokens.refresh_token) {
-        sessionStorage.setItem('accessToken', tokens.access_token);
-        sessionStorage.setItem('refreshToken', tokens.refresh_token);
-        const expirationTimestamp = new Date(
-          Date.now() + this.expirationTime
-        ).getTime();
-        sessionStorage.setItem(
-          'spotifyAccessTokenExpirationTimestamp',
-          expirationTimestamp.toString()
-        );
-        console.log('expirationTimestamp ' + expirationTimestamp);
-      }
-      history.replaceState(
-        null,
-        null,
-        window.location.pathname + window.location.search
-      );
-    }
-  }
-
-  getExpirationTimestamp(): number {
-    const timestamp = sessionStorage.getItem(
-      'spotifyAccessTokenExpirationTimestamp'
-    );
-    return timestamp ? parseInt(timestamp, 10) : 0;
-  }
-
-  hasTokenExpired(): boolean {
-    const currentTime = Date.now();
-    const expirationTimestamp = this.getExpirationTimestamp();
-    return currentTime > expirationTimestamp;
-  }
-
-  private parseFragment(fragment: string): any {
-    const params = new URLSearchParams(fragment);
-    const access_token = params.get('access_token');
-    const refresh_token = params.get('refresh_token');
-    return { access_token, refresh_token };
-  }
-
   getTopArtists(): void {
     const url = `https://api.spotify.com/v1/me/top/artists?limit=50&time_range=${this.timePeriod}`;
 
