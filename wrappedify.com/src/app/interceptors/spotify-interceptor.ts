827e90f2443ee7835c52877436d06ca34fc0dce7
FMcA <f.mcardle1@universityofgalway.ie>
2024-05-25 20:35:22 +0100

    Lefresh

--- wrappedify.com/src/app/interceptors/spotify-interceptor.ts
+++ wrappedify.com/src/app/interceptors/spotify-interceptor.ts
@@ -4,9 +4,10 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
+  HttpErrorResponse,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { switchMap, catchError } from 'rxjs/operators';
 import { AuthServiceService } from '../services/auth-service.service';
 
 @Injectable()
@@ -20,14 +21,34 @@ export class SpotifyInterceptor implements HttpInterceptor {
     return this.authService.getAccessToken().pipe(
       switchMap((token) => {
         if (token) {
-          console.log('Remove this soon. TOKEN: ' + token);
           request = request.clone({
             setHeaders: {
               Authorization: `Bearer ${token}`,
             },
           });
         }
-        return next.handle(request);
+        return next.handle(request).pipe(
+          catchError((error: HttpErrorResponse) => {
+            if (error.status === 401) {
+              // If we get a 401 unauthorized response, attempt to refresh the token
+              return this.authService.getAccessToken().pipe(
+                switchMap((newToken) => {
+                  if (newToken) {
+                    request = request.clone({
+                      setHeaders: {
+                        Authorization: `Bearer ${newToken}`,
+                      },
+                    });
+                    return next.handle(request);
+                  } else {
+                    return throwError(error);
+                  }
+                })
+              );
+            }
+            return throwError(error);
+          })
+        );
       })
     );
   }
